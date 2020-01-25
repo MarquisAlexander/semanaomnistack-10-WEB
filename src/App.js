5395e75e3aa87701a9b552905f50bdf5fa6ad46b
MarquisAlexander <marquissantos123@gmail.com>
2020-01-25 11:58:39 -0300

    contruindo interface

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
@@ -35,8 +36,58 @@ function App() {
 
         </form>
      </aside>
-     <main>
 
+     <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
+              <div className="user-info">
+                <strong>Marquis Alexander</strong>
+                <span>ReactJS, React native, NodeJS</span>
+              </div>
+            </header>
+            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
+              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
+            </p>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
+              <div className="user-info">
+                <strong>Marquis Alexander</strong>
+                <span>ReactJS, React native, NodeJS</span>
+              </div>
+            </header>
+            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
+              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
+            </p>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
+              <div className="user-info">
+                <strong>Marquis Alexander</strong>
+                <span>ReactJS, React native, NodeJS</span>
+              </div>
+            </header>
+            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
+              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
+            </p>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
+              <div className="user-info">
+                <strong>Marquis Alexander</strong>
+                <span>ReactJS, React native, NodeJS</span>
+              </div>
+            </header>
+            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
+              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
+            </p>
+          </li>
+        </ul>
      </main>
    </div>
   );
