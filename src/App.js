402782e1b4479aac96cc39b3aa22bd92390e158c
MarquisAlexander <marquissantos123@gmail.com>
2020-01-29 18:36:47 -0300

    atualização 29/01/2020 - organizando os o código

--- src/App.js
+++ src/App.js
@@ -6,152 +6,40 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs(){
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    )
-  }, [] );
+      setDevs(response.data);
+    }
 
-  useEffect(() => {
-
-  }, );
+    loadDevs();
+  }, []);
   
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
 
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude,
-    })
-    
-    setGithubUsername('');
-    setTechs('');
+    setDevs([...devs, response.data]);
   }
 
   return (
-   <div id="App">
+   <div id="app">
      <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-          <label htmlFor="github_username">Usuário do github</label>
-          <input 
-            name="github_username"
-            id="username_github"
-            required 
-            value={github_username}
-            onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-          <div className="input-block">
-          <label htmlFor="techs">Teclologias</label>
-          <input 
-            name="techs"
-            id="techs"
-            required 
-            value={techs}
-            onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-          <div className="input-block">
-          <label htmlFor="latitude">Latitude</label>
-          <input 
-            type="number"
-            name="latitude"
-            id="latitude"
-            required value={latitude} 
-            onChange={e => setLatitude(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-          <label htmlFor="longitude">Longitude</label>
-          <input 
-            type="number"
-            name="longitude"
-            id="latitude"
-            required value={longitude}
-            onChange={e => setLongitude(e.target.value)}
-            />
-          </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-
-        </form>
+        <DevForm onSubmit={handleAddDev} />
      </aside>
 
      <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
-              <div className="user-info">
-                <strong>Marquis Alexander</strong>
-                <span>ReactJS, React native, NodeJS</span>
-              </div>
-            </header>
-            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
-              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
-            </p>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
-              <div className="user-info">
-                <strong>Marquis Alexander</strong>
-                <span>ReactJS, React native, NodeJS</span>
-              </div>
-            </header>
-            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
-              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
-            </p>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
-              <div className="user-info">
-                <strong>Marquis Alexander</strong>
-                <span>ReactJS, React native, NodeJS</span>
-              </div>
-            </header>
-            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
-              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
-            </p>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/51330232?s=460&v=4" alt="Marquis Alexander"/>
-              <div className="user-info">
-                <strong>Marquis Alexander</strong>
-                <span>ReactJS, React native, NodeJS</span>
-              </div>
-            </header>
-            <p>Desenvolvedor mobile que adora aplicar seu conhecimento para resolver problemas, criar produtos incríveis e impactar vidas.
-              <a href="https://github.com/MarquisAlexander">Acessar perfil no github</a>
-            </p>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
      </main>
    </div>
